cb07b9c75736fa6eac460e181e830c9c5c4bebc1
Daniel <daniel.bliss@mckennaconsultants.com>
2021-11-05 09:26:54 +0000

    Added csv

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,38 @@
-exports.createPages = async ({ actions }) => {
+const path = require("path")
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
-  })
+
+  const result = await graphql(
+    `
+      {
+        allExampleDataCSV {
+          nodes {
+            id
+            firstname
+            lastname
+            email
+          }
+        }
+      }
+    `
+  )
+
+  if (result.errors) {
+    reporter.panicOnBuild(
+      `There was an error loading your Contentful posts`,
+      result.errors
+    )
+    return
+  }
+
+  const data = result.data.allExampleDataCSV.nodes
+
+  if (data.length > 0) {
+    data.forEach((row, index) => {
+      if (row !== null && row !== undefined) {
+        console.log("Create page for " + row.id)
+      }
+    })
+  }
 }
